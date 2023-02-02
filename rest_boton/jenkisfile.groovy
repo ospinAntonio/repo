d1ae7e1df8a01dfafec14d7b13ade33d700bcb8e
ospinAntonio <124282289+ospinAntonio@users.noreply.github.com>
2023-02-02 16:29:50 -0500

    Update jenkisfile.groovy
    .

--- rest_boton/jenkisfile.groovy
+++ rest_boton/jenkisfile.groovy
@@ -1,46 +1,24 @@
-pipeline {
-    agent any
+pipeline { 
+    agent any 
+    options {
+        skipStagesAfterUnstable()
+    }
     stages {
-         stage('Build') {
-            steps {
+        stage('Build') { 
+            steps { 
                 sh 'make' 
-                archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true 
             }
         }
-        stage('Test') {
+        stage('Test'){
             steps {
-                /* `make check` returns non-zero on test failures,
-                * using `true` to allow the Pipeline to continue nonetheless
-                */
-                sh 'make check || true' 
-                junit '**/target/*.xml' 
+                sh 'make check'
+                junit 'reports/**/*.xml' 
             }
         }
-         
-         stage('Test-publicar'){
-         steps {
-             sh 'make check'
-             junit 'reports/pruebas/prueba.xml'
-             }
-         }
-         stage('public-toDockerhub') {
-         when {
-             branch 'master'
-             }
-             steps {
-         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: jenkins_registry_cred_id, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-             sh "docker login -e ${docker_email} -u ${env.USERNAME} -p ${env.PASSWORD} ${docker_registry_url}"
-             }
-             }
-         }
-     stage('Deploy-qa') {
-         when {
-             branch 'master'
-             }
-             steps {
-             sh 'echo publish'
-             sh 'kubeclt apply -f ingress.yaml'
-         }
-     }
-     }
+        stage('Deploy') {
+            steps {
+                sh 'make publish'
+            }
+        }
+    }
 }
