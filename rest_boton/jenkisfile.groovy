443293870bdd1e995023d17aecbb7d51b406f7eb
ospinAntonio <124282289+ospinAntonio@users.noreply.github.com>
2023-02-02 16:58:15 -0500

    Update jenkisfile.groovy
    .

--- rest_boton/jenkisfile.groovy
+++ rest_boton/jenkisfile.groovy
@@ -6,7 +6,7 @@ pipeline {
     stages {
         stage('Build') {
           when {
-                branch '*'
+                branch 'master'
             }
             steps {
                 sh 'java -version'
