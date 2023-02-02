1f54f826d8626631eda8fcddc5de68c52b60e71e
ospinAntonio <124282289+ospinAntonio@users.noreply.github.com>
2023-02-02 16:41:57 -0500

    Update jenkisfile.groovy
    .

--- rest_boton/jenkisfile.groovy
+++ rest_boton/jenkisfile.groovy
@@ -21,6 +21,21 @@ pipeline {
                 junit 'reports/**/*.xml'
             }
         }
+        stage('Test-veracode'){
+        when {
+                branch 'master'
+            }
+            steps {
+                sh 'make check'
+                junit 'reports/**/*.xml'
+            }
+     }
+         stage('Test-publicar'){
+         steps {
+             sh 'make check'
+             junit 'reports/**/*.xml'
+             }
+         }
         stage('Deploy') {
             steps {
                 sh 'make publish'
