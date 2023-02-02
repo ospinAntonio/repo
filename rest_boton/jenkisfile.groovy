43a941005315874180d33edfca233f66b032c075
ospinAntonio <124282289+ospinAntonio@users.noreply.github.com>
2023-02-02 16:53:47 -0500

    Update jenkisfile.groovy
    .

--- rest_boton/jenkisfile.groovy
+++ rest_boton/jenkisfile.groovy
@@ -43,23 +43,4 @@ pipeline {
             }
         }
     }
-         stage('public-toDockerhub') {
-         when {
-             branch 'dev'
-             }
-             steps {
-         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: jenkins_registry_cred_id, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-             sh "docker login -e ${docker_email} -u ${env.USERNAME} -p ${env.PASSWORD} ${docker_registry_url}"
-             }
-             }
-         }
-     stage('Deploy-qa') {
-         when {
-             branch 'master'
-             }
-             steps {
-             sh 'echo publish'
-             sh 'kubeclt apply -f ingress.yaml'
-         }
-     }
 }
