cca5dae2d0ebd22b35ff1a2649c7963fcab4b832
ospinAntonio <124282289+ospinAntonio@users.noreply.github.com>
2023-02-02 14:59:08 -0500

    Update jenkisfile.groovy
    arreglos en jenkisnfile

--- jenkisfile.groovy
+++ jenkisfile.groovy
@@ -1,39 +1,60 @@
 pipeline {
     agent any
-    tools {
-        gradle "gradle-7.5.1"
+    options {
+        skipStagesAfterUnstable()
     }
     stages {
         stage('Build') {
-                steps {
-                    sh 'gradle task'
-                    sh 'gradle init'
-                    sh 'gradle wrapper'
-                    sh './gradle build'
-                }
+            when {
+                branch ''
+            }
+            steps {
+                sh 'java -version'
+                sh 'npm run build:dev'
             }
         }
-            stage('Test'){
-                steps {
-                   echo 'test succesfull'
-                }
+        stage('Test-sonar'){
+        when {
+                branch 'master'
+            }
+            steps {
+                sh 'make check'
+                junit 'reports/**/*.xml'
+            }
         }
-             stage('Build docker image'){
-                steps {
-                   echo 'succesfull'
-                }
-         }
-             stage('public-toDockerhub') {
-                 steps {
-             withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: jenkins_registry_cred_id, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-                 sh "docker login -e ${docker_email} -u ${env.USERNAME} -p ${env.PASSWORD} ${docker_registry_url}"
-                 sh 'docker push semillero:latest'
-                 }
-                 }
-             }
-         stage('push docker imag') {
-                 steps {
-                    echo 'succesfull'
-             }
-         }
+            stage('Test-veracode'){
+        when {
+                 branch 'master'
+            }
+            steps {
+                sh 'make check'
+                junit 'reports/**/*.xml'
+            }
+        }
+        stage('Test-publicar'){
+            steps {
+                sh 'make check'
+                junit 'reports/**/*.xml'
+            }
+        }
+        stage('public-toDockerhub') {
+        when {
+            branch 'master'
+        }
+        steps {
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: jenkins_registry_cred_id, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+            sh "docker login -e ${docker_email} -u ${env.USERNAME} -p ${env.PASSWORD} ${docker_registry_url}"
+            }
+            }
+        }
+        stage('Deploy-qa') {
+        when {
+                branch 'master'
+            }
+            steps {
+                sh 'echo publish'
+                sh 'kubeclt apply -f ingress.yaml'
+            }
+        }
+    }
 }
