b7b5229c62d3359f1f3bc52c71d8649f53749f18
ospinAntonio <124282289+ospinAntonio@users.noreply.github.com>
2023-02-15 08:43:55 -0500

    Update jenkisfile.groovy

--- rest_boton/jenkisfile.groovy
+++ rest_boton/jenkisfile.groovy
@@ -20,6 +20,7 @@ pipeline {
         script{
           withSonarQubeEnv('sonar') {    
             sh """
+            cd rest_boton \
             ./gradlew sonarqube \
              -Dsonar.projectKey=sonar \
              -Dsonar.host.url=http://localhost:9000 \
