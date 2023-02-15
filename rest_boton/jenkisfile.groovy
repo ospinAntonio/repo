93fed68c2ef2cd522f39cf5ddde457229a6a2267
ospinAntonio <124282289+ospinAntonio@users.noreply.github.com>
2023-02-15 09:58:33 -0500

    Update jenkisfile.groovy

--- rest_boton/jenkisfile.groovy
+++ rest_boton/jenkisfile.groovy
@@ -25,12 +25,9 @@ pipeline {
               -Dsonar.projectKey=sonar \
               -Dsonar.host.url=http://localhost:9000 \
               -Dsonar.login=6773b36a0d6d32d9e1a26ef66e4a59b855d374f7 """
-          }    
+          }
+        }
       }
-      steps {
-                sh 'make check'
-                junit 'reports/**/*.xml' 
-            }
     }
     stage('Build Docker Image') {
       steps {
