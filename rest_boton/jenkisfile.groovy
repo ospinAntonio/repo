be938a75748091a85e283611cea6e7ddb52ac7c7
ospinAntonio <124282289+ospinAntonio@users.noreply.github.com>
2023-02-15 09:45:42 -0500

    Update jenkisfile.groovy

--- rest_boton/jenkisfile.groovy
+++ rest_boton/jenkisfile.groovy
@@ -24,8 +24,8 @@ pipeline {
             ./gradlew sonarqube \
               -Dsonar.projectKey=sonar \
               -Dsonar.host.url=http://localhost:9000 \
-              -Dsonar.login=6773b36a0d6d32d9e1a26ef66e4a59b855d374f7 
-                make check
+              -Dsonar.login=6773b36a0d6d32d9e1a26ef66e4a59b855d374f7 \
+                make check \
                 junit 'reports/**/*.xml """"
           }       
         }
