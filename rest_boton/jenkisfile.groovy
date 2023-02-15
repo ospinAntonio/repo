e8d8a55b4a69958f54b06fcadcd2039a35a3e45e
ospinAntonio <124282289+ospinAntonio@users.noreply.github.com>
2023-02-15 09:44:39 -0500

    Update jenkisfile.groovy

--- rest_boton/jenkisfile.groovy
+++ rest_boton/jenkisfile.groovy
@@ -19,15 +19,14 @@ pipeline {
       steps {
         script{
           withSonarQubeEnv('sonar') {    
-            sh """
+            sh """"
             cd rest_boton \
             ./gradlew sonarqube \
               -Dsonar.projectKey=sonar \
               -Dsonar.host.url=http://localhost:9000 \
-              -Dsonar.login=6773b36a0d6d32d9e1a26ef66e4a59b855d374f7 """
-            sh "
+              -Dsonar.login=6773b36a0d6d32d9e1a26ef66e4a59b855d374f7 
                 make check
-                junit 'reports/**/*.xml "
+                junit 'reports/**/*.xml """"
           }       
         }
       }
